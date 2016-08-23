36a2b7092e0b09296f0adde36f2ed3b0aefb5589
rgtm02-mic <antonin.blot@gmail.com>
2016-08-23 12:27:05 +0200

    Create channel map can create linear or tetrode channel maps

--- createChannelMapFile.m
+++ createChannelMapFile.m
@@ -1,13 +1,19 @@
-function [sChannelMapFileName] = createChannelMapFile(nNumChannels, sRootPath)
+function [sChannelMapFileName] = createChannelMapFile(nNumChannels, ...
+    sRootPath, bTetrode)
 
 connected = ones(nNumChannels, 1);
 chanMap   = 1:nNumChannels;
 chanMap0ind = chanMap - 1;
 
-xcoords   = repmat([1 2 3 4]', 1, nNumChannels/4);
-xcoords   = xcoords(:);
-ycoords   = repmat(1:nNumChannels/4, 4, 1);
-ycoords   = ycoords(:);
+if bTetrode
+    xcoords   = repmat([1 2 3 4]', 1, nNumChannels/4);
+    xcoords   = xcoords(:);
+    ycoords   = repmat(1:nNumChannels/4, 4, 1);
+    ycoords   = ycoords(:);
+else % put the channel in a line
+    xcoords   = zeros(nNumChannels, 1);
+    ycoords   = (1:nNumChannels);
+end
 kcoords   = ones(nNumChannels,1); % grouping of channels (i.e. tetrode groups)
 
 sChannelMapFileName = fullfile(sRootPath, 'chanMap.mat');
